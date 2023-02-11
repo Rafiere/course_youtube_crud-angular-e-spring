295f797ee1e3799fe90b18524b5974e55a4ec065
Rafiere <rafapgbr@gmail.com>
2023-02-11 18:33:06 -0300

    feat: add custom light theme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /* Se tivermos apenas o "localhost", vazio, redirecionaremos para o "/courses". */
+  {path: '', redirectTo: 'courses', pathMatch: 'full'},
+
+  /* Essa será uma rota de lazy loading. Isso significa que a aplicação só carregará esse módulo quando
+  * ele for necessário. Isso melhora a performance e reduz o tempo de carregamento em geral. */
+  {path: 'courses', loadChildren: () => import('./courses/courses.module').then(module => module.CoursesModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
