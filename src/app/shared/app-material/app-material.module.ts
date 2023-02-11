33c888048a9629c33b87635e00a6feeb7b4fd90f
Rafiere <rafapgbr@gmail.com>
2023-02-11 20:11:40 -0300

    feat: add material module

---
+++ src/app/shared/app-material/app-material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import {MatTableModule} from "@angular/material/table";
+import {MatCardModule} from "@angular/material/card";
+import {MatToolbarModule} from "@angular/material/toolbar";
+
+/* Esse módulo será utilizado para organizarmos os imports do Angular Material. Assim, se criarmos
+* outros módulos, basta importarmos esse módulo e, dessa forma, teremos todos os imports do Angular
+* Material, sem precisarmos ficar importando, novamente, todos os componentes em cada módulo que
+* o utilizarmos. */
+@NgModule({
+  exports: [MatTableModule, MatCardModule, MatToolbarModule]
+})
+export class AppMaterialModule { }
