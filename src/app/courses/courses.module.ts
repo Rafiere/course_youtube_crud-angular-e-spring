76b84319a37def94dbae1aebacb35344fb12a1fa
Rafiere <rafapgbr@gmail.com>
2023-02-12 07:26:45 -0300

    feat: add get courses

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {CoursesComponent} from "./courses/courses.component";
 import {AppMaterialModule} from "../shared/app-material/app-material.module";
+import {CoursesRoutingModule} from "./courses-routing.module";
 
 @NgModule({
   declarations: [
@@ -9,6 +10,7 @@ import {AppMaterialModule} from "../shared/app-material/app-material.module";
   ],
   imports: [
     CommonModule,
+    CoursesRoutingModule,
     AppMaterialModule
   ]
 })
--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import {Course} from "../models/course";
+import {CoursesService} from "../services/courses.service";
+import {Observable} from "rxjs";
 
+/* Isso é importante para gerar o bundle da aplicação. */
 @Component({
-  selector: 'app-courses',
-  templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  selector: 'app-courses', //Aqui temos a diretiva, ou seja, o nome da tag do HTML que utilizaremos esse componente.
+  templateUrl: './courses.component.html', //É o endereço do arquivo em que temos o HTML para esse componente.
+  styleUrls: ['./courses.component.scss'] //Aqui temos uma referência para os arquivos de CSS do componente.
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[]  = [{
-    _id: "1", name: "Angular", category: "front-end"
-  }];
+  /* O componente não sabe os dados que serão renderizados. O serviço que fará a obtenção desses dados. */
+  courses: Observable<Course[]>;
   displayedColumns = ['name', 'category']
-  constructor() { }
+
+  /* Estamos fazendo a injeção de dependências. */
+  constructor(private coursesService: CoursesService) {
+    this.courses = this.coursesService.list()
+  }
 
   ngOnInit(): void {
+
   }
 
 }
