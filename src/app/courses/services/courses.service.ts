d396e8f55bceb5145eb8f53dc1369a02834384a4
Rafiere <rafapgbr@gmail.com>
2023-02-12 07:26:27 -0300

    feat: add courses service

---
+++ src/app/courses/services/courses.service.ts
@@ -0,0 +1,34 @@
+import {Injectable} from '@angular/core';
+import {Course} from "../models/course";
+import {HttpClient} from "@angular/common/http";
+import {first, tap} from "rxjs/operators";
+
+/* Os decorators adicionam informações sobre o ciclo de vida de uma determinada classe no Angular. */
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+
+  private readonly API = '/assets/courses.json'
+
+
+  /* Toda a manipulação de dados relacionado aos cursos será feita nessa classe. É nessa classe que
+  * ajustaremos os dados para virem corretamente no servidor. */
+
+  /* A injeção de dependências está sendo feita pelo construtor. Estamos dizendo para o Angular que
+  * queremos uma dependência do HTTP Client, e o Angular ficará responsável por fornecer a instância
+  * dessa classe. */
+
+  /* Quando declaramos o "HttpClientModule" no "app.module.ts", o Angular saberá que a instância que será
+  * criada deverá ser pega dentro do AppModule. */
+  constructor(private httpClient: HttpClient) {
+  }
+
+  list() {
+
+    /* Estamos retornando um objeto do tipo Observable<Course[]>*/
+
+    return this.httpClient.get<Course[]>(this.API).pipe(tap(courses => console.log(courses)));
+  }
+}
